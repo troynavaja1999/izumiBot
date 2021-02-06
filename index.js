29cb95e5fde3d226a2bbc167251d5eea8ccdfcb2
Troy Navaja <tbnavaja07@gmail.com>
2021-02-06 21:35:50 +0800

    changed index to join voice channel

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const config = require('./config.json')
 
-client.on('ready', () => {
-    console.log('The client is ready!')
+client.on('message', async message => {
+    if (!message.guild) return;
+
+    if (message.content === 'we love you izumi') {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+        } else {
+            message.reply('saang vc ako tatambay?')
+        }
+    }
 })
 
-client.login(process.env.IZUMI_TOKEN)
+// client.login(process.env.IZUMI_TOKEN)
+client.login(config.token)
